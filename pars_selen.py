daaa023f889973376215f174e79ae3bf27f2ef5d
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-01 20:07:28 +0300

    working on parser 2

--- pars_selen.py
+++ pars_selen.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
 
 # Путь к драйверу Chrome WebDriver
 driver_path = 'C:\PyProjects\data_analysis\dafiles\chromedriver\chromedriver'
@@ -20,17 +23,27 @@ def parse_element(url):
         # Открытие страницы
         driver.get(url)
 
-        # Нахождение элемента на странице
-        wait = WebDriverWait(driver, 5)
-        element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.page-title-count-wQ7pG')))
+        # Проверка наличия редиректа на новую страницу
+        if driver.current_url != url:
+            print(f"Редирект на новую страницу: {driver.current_url}")
+            driver.quit()
+            return '0'
+        else:
+            # Нахождение элемента на странице
+            wait = WebDriverWait(driver, 5)
+            element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.page-title-count-wQ7pG')))
 
-        # Извлечение текста из элемента
-        text = element.text
+            # Извлечение текста из элемента
+            text = element.text
 
-        # Закрытие браузера
+            # Закрытие браузера
+            driver.quit()
+            return text
+
+    except TimeoutException:
+        print(f"Редирект на новую страницу: {url}")
         driver.quit()
-
-        return text
+        return '0'
     except Exception as e:
         print(f"Ошибка при обработке URL: {url}")
         print(f"Ошибка: {str(e)}")
@@ -44,7 +57,7 @@ def process_csv(input_file, output_file):
         rows = list(reader)
 
         for row in rows:
-            url = f'https://www.{row[2]}+-%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D1%82%D0%B8+-ddr3+-%2215.6%22&user=1'
+            url = f'https://www.{row[2]}+-%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D0%B8+-ddr3+-%D0%B2%D1%8B%D0%B1%D0%BE%D1%80+-%D1%81%D0%BA%D0%BB%D0%B0%D0%B4+-%D1%81%D0%BA%D0%BB%D0%B0%D0%B4%D0%B0+-buy+-%D1%80%D0%B0%D0%B4%D1%8B&user=1'
 
             # Парсинг элемента на странице
             parsed_value = parse_element(url)
