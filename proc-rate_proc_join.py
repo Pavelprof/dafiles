ad362b85948452830abba77eeab1dcff5ccbe117
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-03 10:23:56 +0300

    joining 2

--- proc-rate_proc_join.py
+++ proc-rate_proc_join.py
@@ -15,7 +15,7 @@ for _, row in output_all_data.iterrows():
     if not matching_rows.empty:
         best_match_row = matching_rows.iloc[0]  # Берем первую найденную строку
         merged_row = pd.concat([row, best_match_row], axis=0)
-        merged_data = merged_data.append(merged_row, ignore_index=True)
+        merged_data = merged_data._append(merged_row, ignore_index=True)
 
 # Сохранение результата в новый файл
 merged_data.to_excel(r'C:\Users\Павел\Desktop\output_merged.xlsx', index=False)
