15b2968d0da26633cc9f9b947f308cb9d718d093
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-08-21 19:15:54 +0300

    testing APIs

--- api_moex_test.py
+++ api_moex_test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_moex_bid_price(ticker):
+def get_moex_quote(ticker):
     base_url = f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQTF/securities/{ticker}.json?iss.meta=off&iss.json=extended"
 
     response = requests.get(base_url)
@@ -15,10 +15,35 @@ def get_moex_bid_price(ticker):
     return None
 
 
-ticker = "INGO"
-bid_price = get_moex_bid_price(ticker)
+import requests
 
-if bid_price is not None:
-    print(f"BID Price of {ticker}: {bid_price}")
+
+def get_quotes_from_moex(ticker_list):
+    def get_moex_quote(ticker):
+        base_url = f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQTF/securities/{ticker}.json?iss.meta=off&iss.json=extended"
+        response = requests.get(base_url)
+        data = response.json()
+
+        if len(data) > 1 and 'marketdata' in data[1]:
+            marketdata = data[1]['marketdata'][0]
+            bid_price = marketdata.get('BID')
+            last_price = marketdata.get('LAST')
+
+            # Если BID отсутствует или равен None, возвращаем LAST. Иначе возвращаем BID.
+            return bid_price if bid_price is not None else last_price
+        return None
+
+    quotes = {}
+    for ticker in ticker_list:
+        quotes[ticker] = get_moex_quote(ticker)
+
+    return quotes
+
+
+ticker = ["INGO", "TGRN"]
+price = get_quotes_from_moex(ticker)
+
+if price is not None:
+    print(f"BID Price of {ticker}: {price}")
 else:
     print(f"Failed to retrieve BID price for {ticker}")
--- api_tinkoff_test.py
+++ api_tinkoff_test.py
@@ -3,25 +3,37 @@ from tinkoff.invest import Client, InstrumentIdType
 
 TOKEN = os.environ["TIN_API_KEY"]
 
+def get_prices_by_figi(figi_list):
+    with Client(TOKEN) as client:
+        r = client.market_data.get_market_search_by_figi(figi=figi_list)
+        prices = {}
+        for item in r.payload.instruments:
+            if item.figi in figi_list:
+                prices[item.figi] = item.last_price
 
-with Client(TOKEN) as client:
-
-    res = client.instruments.etf_by(
-        id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
-        class_code='TQTF',
-        id = 'TRUR')
-    print(res)
+        return prices
 
 
-#last price by figi
+
+# figi by ticker and class_code
 # with Client(TOKEN) as client:
-#     r = client.market_data.get_last_prices(figi=['BBG0120X0XX2','BBG004731032','BBG004S68B31',])
-#     print(r)
-#     print(type(r))
-#     print(r.last_prices[0])
-#     print(type(r.last_prices[0]))
-#     print(r.last_prices[0].price)
-#     print(type(r.last_prices[0].price))
-#     p = r.last_prices[0].price
-#     price = p.units + p.nano / 1e9
-#     print(price)
+#
+#     res = client.instruments.etf_by(
+#         id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
+#         class_code='TQTF',
+#         id = 'TRUR')
+#     print(res)
+
+
+# last price by figi
+with Client(TOKEN) as client:
+    r = client.market_data.get_last_prices(figi=['BBG0120X0XX2','BBG004731032','BBG004S68B31',])
+    print(r)
+    print(type(r))
+    print(r.last_prices[0])
+    print(type(r.last_prices[0]))
+    print(r.last_prices[0].price)
+    print(type(r.last_prices[0].price))
+    p = r.last_prices[0].price
+    price = p.units + p.nano / 1e9
+    print(price)
