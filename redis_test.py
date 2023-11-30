87b1e836cff4b3ba26170987ef874bd9fc20e4d8
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-11-30 18:28:15 +0300

    testing redis 2

--- redis_test.py
+++ redis_test.py
@@ -1,19 +1,29 @@
 import redis
 
 # redis cash db
-# r = redis.Redis(host='localhost', port=6379, db=0)
-#
-# r.setex('my_key', 20, 'Hello, Redis!')
-# r.setex('my_key1', 20, 'Hello, Redis!1')
-#
-# for key in r.scan_iter("*"):
-#     print(key)
+rc = redis.Redis(host='localhost', port=6379, db=0)
 
 # redis mq db
-r = redis.Redis(host='localhost', port=6379, db=1)
+rq = redis.Redis(host='localhost', port=6379, db=1)
 
-# r.setex('my_key', 20, 'Hello, Redis!')
-# r.setex('my_key1', 20, 'Hello, Redis!1')
+# refresh redis redis db
+# rc.flushdb()
+# rq.flushdb()
 
-for key in r.scan_iter("*"):
+
+# rc.setex('my_key', 20, 'Hello, Redis!')
+# rc.setex('my_key1', 20, 'Hello, Redis!1')
+#
+print('data in cash')
+for key in rc.scan_iter("*"):
+    value = rc.get(key)
+    print(f"{key}: {value}")
+
+print(' ')
+
+# rq.setex('my_key', 20, 'Hello, Redis!')
+# rq.setex('my_key1', 20, 'Hello, Redis!1')
+
+print('data in mq')
+for key in rq.scan_iter("*"):
     print(key)
